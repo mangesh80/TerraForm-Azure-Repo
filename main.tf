03f2bbdab5af5810962590ad9e2341fd1f51fba6
Mangesh Prabhu <mangesh.cgi@gmail.com>
2024-01-18 10:43:01 -0500

    Added a comment

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
+#Adding a comment to update the file
 terraform {
   required_providers {
     azurerm = {
